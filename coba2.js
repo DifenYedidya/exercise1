439ee432410c26794b2db6b04f7f894dc1b90889
zeknier <46747165+zeknier@users.noreply.github.com>
2019-02-28 19:02:52 +0700

    Add files via upload

---
+++ coba2.js
@@ -0,0 +1,11 @@
+var ar = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10];
+var jumlah = 0;
+var i = 0;
+
+while(i<10){
+    if ((ar[i] % 2) === 1){
+        console.log(ar[i]);
+                
+    }
+    i++;
+}
